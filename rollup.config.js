3a31d9dd83d29cb1ed4bad917050ad1be18961a7
TonySapa <antoniosapa90@gmail.com>
2022-05-29 03:42:23 +0200

    Storybooks is fixed, and works again

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "esm" },
       {
-        file: "example/src/reactComponentLib/index.js",
+        file: "es/src/reactComponentLib/index.js",
         format: "es",
         banner: "/* eslint-disable */"
       }
