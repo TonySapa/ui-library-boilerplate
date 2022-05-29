9948ce61ed5c1bf996a64b02bbdc9a075de290fb
TonySapa <antoniosapa90@gmail.com>
2022-05-29 03:30:02 +0200

    Library can be imported into TS project

---
+++ example/src/reactComponentLib/demo/Demo.stories.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+declare const _default: ComponentMeta<({ children }: import("./Demo.types").DemoProps) => JSX.Element>;
+export default _default;
+export declare const Primary: ComponentStory<({ children }: import("./Demo.types").DemoProps) => JSX.Element>;
