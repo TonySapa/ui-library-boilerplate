3236c8054b7a2de6e561d786223518d221ee8425
TonySapa <antoniosapa90@gmail.com>
2022-05-30 00:41:28 +0200

    Static files deleted before pushes to github

--- dist/sora-theme/SoraTheme.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { SoraThemeProps } from './SoraTheme.types';
-declare const _default: ComponentMeta<({ children, theme }: SoraThemeProps) => JSX.Element>;
-export default _default;
-export declare const DarkButtons: ComponentStory<({ children, theme }: SoraThemeProps) => JSX.Element>;
--- dist/sora-theme/SoraTheme.types.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-import { Theme } from '../_types';
-export declare type SoraThemeProps = {
-    children: ReactNode;
-    theme: Theme;
-};
